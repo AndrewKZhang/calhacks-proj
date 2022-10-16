635e11535f77d7ec45fa7457ae6dd1a17290c0f1
Andrew Zhang <andrewkzhang@berkeley.edu>
2022-10-16 12:49:15 -0700

    react frontend

--- new-react/anywhere/src/App.js
+++ new-react/anywhere/src/App.js
@@ -1,11 +1,31 @@
+import React, { useState } from 'react'
 import Header from "./components/Header"
-import './App.css';
+import SearchBar from "./components/SearchBar"
+import PlaceData from "./Data.json"
+import DatePicker from "react-datepicker"
+import 'react-datepicker/dist/react-datepicker.css'
+import './App.css'
+
 
 function App() {
+  const[selectedDate, setSelectedDate] = useState(null)
   return (
     <div className="App">
       <Header />
-      
+      <SearchBar placeholder="Enter your origin..." data={PlaceData}/>
+      <SearchBar placeholder="Enter your destination..." data={PlaceData}/>
+      <DatePicker 
+      selected={selectedDate} 
+      onchange={date =>setSelectedDate(date)} 
+      dateFormat = 'yyyy/MM/dd'
+      minDate = {new Date()}
+      isClearable
+      showYearDropdown
+      scrollableYearDropdown
+      scrollableMonthYearDropdown
+      />
+
+
     </div>
   );
 }
